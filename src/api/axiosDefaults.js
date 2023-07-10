092b9e6621c011dd96f30b070559a72debe85486
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-07-10 19:47:51 +0000

    Link api and create sign up form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://djangorestframework-darko-070f41296928.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
