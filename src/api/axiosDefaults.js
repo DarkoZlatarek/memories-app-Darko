d65398b7a9c9b592b32052da1a75d178ecfd1a08
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-07-11 19:27:58 +0000

    Add axiosReq and xiosRes to refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from 'axios'
 axios.defaults.baseURL = 'https://djangorestframework-darko-070f41296928.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
