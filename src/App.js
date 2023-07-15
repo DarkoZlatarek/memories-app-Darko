195f0e5ecb2266e74bad1a84836d531428ca5502
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-07-15 10:20:28 +0000

    Add post page, like and unlike method

--- src/App.js
+++ src/App.js
@@ -6,8 +6,29 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
+import PostPage from "./pages/posts/PostPage";
+import { useParams } from "react-router-dom/cjs/react-router-dom";
+import { useEffect, useState } from "react";
+import { axiosReq } from "./api/axiosDefaults";
 
 function App() {
+  const {id} = useParams();
+  const [post, setPost] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    handleMount();
+  }, [id])
 
   return (
     <div className={styles.App}>
@@ -18,6 +39,7 @@ function App() {
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route render={() => <h1>Page not found!</h1>} />
         </Switch>
       </Container>
