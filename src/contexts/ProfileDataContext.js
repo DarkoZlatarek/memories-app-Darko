24aeb727df398f73b17f82584ff62d71d7b0919d
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-07-22 09:32:52 +0000

    Add profile data context

---
+++ src/contexts/ProfileDataContext.js
@@ -0,0 +1,48 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
+
+export const ProfileDataContext = createContext();
+export const SetProfileDataContext = createContext();
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
+
+export const ProfileDataProvider = ({ children }) => {
+
+  const [profileData, setProfileData] = useState({
+    // We will use pageProfile later
+    pageProfile: { results: [] },
+    popularProfiles: { results: [] },
+  });
+  
+  const currentUser = useCurrentUser;
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-followers_count"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+
+  return (
+    <ProfileDataContext.Provider value={profileData}>
+      <SetProfileDataContext.Provider value={setProfileData}>
+        {children}
+      </SetProfileDataContext.Provider>
+    </ProfileDataContext.Provider>
+  );
+};
+
+export default ProfileDataProvider;
