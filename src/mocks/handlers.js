8645bdd286ce09ba0ff453809cc08ad9f57a9288
Darko Zlatarek <darko.zlatarek@gmail.com>
2023-07-26 20:05:12 +0000

    Add few tests in NavBar.test.js

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,7 +3,21 @@ import { rest } from "msw"
 const baseUrl = "https://djangorestframework-darko-070f41296928.herokuapp.com/"
 
 export const handlers = [
-    rest.get(`${baseUrl}dj-rest-auth/user/`, (req,res,ctx) =>{
-        return res(ctx.json)
-    })
-]
+  rest.get(`${baseUrl}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 4,
+        username: "Darko",
+        email: "",
+        first_name: "",
+        last_name: "",
+        profile_id: 3,
+        profile_image:
+          "https://res.cloudinary.com/dcqcddsyx/image/upload/v1/media/../default_profile_muvyn4",
+      })
+    );
+  }),
+  rest.post(`${baseUrl}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
